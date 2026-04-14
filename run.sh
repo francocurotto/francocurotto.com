bf5b13709cce44a29d6ce7d579636c34f0aa8ba5
Franco Curotto <francocurotto@gmail.com>
2026-04-13 20:31:58 -0400

    remove inline output

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 DIR="$(cd "$(dirname "$0")" && pwd)"
-nginx -p "$DIR" -c "$DIR/nginx/ssi.conf" -g "daemon off;"
+#nginx -p "$DIR" -c "$DIR/nginx/ssi.conf" -g "daemon off;"
+nginx -p "$DIR" -c "$DIR/nginx/ssi.conf"
